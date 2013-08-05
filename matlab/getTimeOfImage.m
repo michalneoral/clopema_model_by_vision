13974bb69c854ac73bd8a07ced23b1ead86cee89
root <root@neosh.(none)>
2013-08-05 10:31:15 +0200

    Change getting time from "time.time" to "header.stamp.time"

--- matlab/getTimeOfImage.m
+++ matlab/getTimeOfImage.m
@@ -1,3 +1,8 @@
 function [ time ] = getTimeOfImage( msgs, n_topic, n_image )
-    time = msgs{n_topic,2}{1,n_image}.time.time;
+
+% get time of image
+% i found two different times, *.header.stamp.time is probably better
+
+    %time = msgs{n_topic,2}{1,n_image}.time.time;
+    time = msgs{n_topic, 1}{1, n_image}.header.stamp.time;
 end
