a08f421a6783be3a4e9f48e2eaf6d5fb08a5565b
root <root@neosh.(none)>
2013-08-08 17:04:16 +0200

    Treatment of making circles

--- matlab/MidpointCircle.m
+++ matlab/MidpointCircle.m
@@ -4,33 +4,36 @@
 % Created : 19th March 2007
 function i = MidpointCircle(i, radius, xc, yc, value)
 
-xc = int16(xc);
-yc = int16(yc);
+    if xc < 640-radius && yc < 480-radius && xc > radius && yc > radius 
 
-x = int16(0);
-y = int16(radius);
-d = int16(1 - radius);
+    xc = int16(xc);
+    yc = int16(yc);
 
-i(xc, yc+y) = value;
-i(xc, yc-y) = value;
-i(xc+y, yc) = value;
-i(xc-y, yc) = value;
+    x = int16(0);
+    y = int16(radius);
+    d = int16(1 - radius);
 
-while ( x < y - 1 )
-    x = x + 1;
-    if ( d < 0 ) 
-        d = d + x + x + 1;
-    else 
-        y = y - 1;
-        a = x - y + 1;
-        d = d + a + a;
+    i(xc, yc+y) = value;
+    i(xc, yc-y) = value;
+    i(xc+y, yc) = value;
+    i(xc-y, yc) = value;
+
+    while ( x < y - 1 )
+        x = x + 1;
+        if ( d < 0 ) 
+            d = d + x + x + 1;
+        else 
+            y = y - 1;
+            a = x - y + 1;
+            d = d + a + a;
+        end
+        i( x+xc,  y+yc) = value;
+        i( y+xc,  x+yc) = value;
+        i( y+xc, -x+yc) = value;
+        i( x+xc, -y+yc) = value;
+        i(-x+xc, -y+yc) = value;
+        i(-y+xc, -x+yc) = value;
+        i(-y+xc,  x+yc) = value;
+        i(-x+xc,  y+yc) = value;
     end
-    i( x+xc,  y+yc) = value;
-    i( y+xc,  x+yc) = value;
-    i( y+xc, -x+yc) = value;
-    i( x+xc, -y+yc) = value;
-    i(-x+xc, -y+yc) = value;
-    i(-y+xc, -x+yc) = value;
-    i(-y+xc,  x+yc) = value;
-    i(-x+xc,  y+yc) = value;
 end
