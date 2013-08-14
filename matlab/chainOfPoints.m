440019b91b5fdd446d40d0ba61693ad3a77c20c8
root <root@neosh.(none)>
2013-08-14 10:46:06 +0200

    Modified to made chain from more points

--- matlab/chainOfPoints.m
+++ matlab/chainOfPoints.m
@@ -1,14 +1,18 @@
-function [ chain ] = chainOfPoints( startPoint, optFlow )
+function [ chainP ] = chainOfPoints( startPoints, optFlow )
+    chainP=cell(size(startPoints,1),size(optFlow,2));
+    for i = 1:size(startPoints,1)
+         chainP(i,:)= getChain(startPoints{i,1}, optFlow);
+    end    
+end
 
-    chain = zeros(size(optFlow,2),2);
-    chain(1,:) = startPoint;
-    
-    for i = 2:size(optFlow,2)
-        chain(i,:)=getNextPoint(chain(i-1,:), optFlow, i);
+function [ chain ] = getChain( startPoint, optFlow )
+    chain=cell(size(optFlow,2),1);
+    chain(1) = {startPoint};
+    for i=2:size(optFlow,2)
+        chain(i) = {getNextPoint(chain(i-1), optFlow, i)};
     end
-
 end
 
 function [ nextPoint ] = getNextPoint(lastPoint, flow, number)
-    nextPoint = [flow{1,number}(lastPoint(1),lastPoint(2),1) + lastPoint(1),flow{1,number}(lastPoint(1),lastPoint(2),2) + lastPoint(2)];
+    nextPoint = [flow{1,number}(lastPoint{1,1}(1),lastPoint{1,1}(2),1) + lastPoint{1,1}(1),flow{1,number}(lastPoint{1,1}(1),lastPoint{1,1}(2),2) + lastPoint{1,1}(2)];
 end
