a57914d83a6d1c52f99b9a3526437a8e90c759f2
michalneoral <michalneoral@email.cz>
2013-11-13 20:53:06 +0100

    New file of local folders

---
+++ src/local_options.py
@@ -0,0 +1,7 @@
+"""Local options"""
+
+"""Folder where is a clopema_collect_model_data"""
+pcglocate='/home/neosh/ros_catkin_ws/src/clopema_model_by_vision/'
+
+"""Folder where will be saved a collected data"""
+savefolder='/media/neosh/5FB92F7D501A5B3A/Clopema/Pokusy/'
